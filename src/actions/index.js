618c02053a250b8cf314727ad96f9ffb38472e2f
Edward-1020 <iezzuyoung@gmail.com>
2019-06-08 17:02:15 +0800

    feat: fetch todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,44 @@ import {
     ADD_TODO,
     TOGGLE_TODO,
     SET_FILTER,
-    SET_TODO_TEXT
+    SET_TODO_TEXT,
+    FETCH_TODOS_REQUEST,
+    FETCH_TODOS_SUCCESS,
+    FETCH_TODOS_FAILURE
 } from './actionTypes';
 
 let nextTodoId = 0;
+
+const fetchTodosRequest = () => ({
+    type: FETCH_TODOS_REQUEST
+})
+
+const fetchTodosSuccess = (data) => ({
+    type: FETCH_TODOS_SUCCESS,
+    data
+})
+
+const fetchTodosFailure = (error) => ({
+    type: FETCH_TODOS_FAILURE,
+    error
+})
+
+export const fetchTodos = () => {
+    return (dispatch) => {
+        dispatch(fetchTodosRequest());
+        return fetch('./mock/todos.json').then(
+            response => {
+                response.json().then(data => {
+                    dispatch(fetchTodosSuccess(data))
+                })
+            },
+            error => {
+                dispatch(fetchTodosFailure(error))
+                console.log("An error occurred" + error)
+            }
+        )
+    }
+}
 
 /**
  * 新增代办事项
